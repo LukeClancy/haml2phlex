35a70d25367121f4f000d1cded44f61db24eacf3
Luke Clancy <lukeclancy@hotmail.com>
2023-02-17 13:10:16 -0800

    minor changes to version 0.1.5

--- lib/haml2phlex.rb
+++ lib/haml2phlex.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative "haml2phlex/version"
 
 module Haml2phlex
--- lib/haml2phlex/version.rb
+++ lib/haml2phlex/version.rb
@@ -1,5 +1,5 @@
 
 	module Haml2phlex
-	    VERSION = '0.1.4'
+	    VERSION = '0.1.5'
 	end
 	#this file gets overwritten automatically on minor updates, major ones need to be manually changed
